1f64c3f1e873a0bae7cc2926b4cfd33ac1d7b1a4
Hafizuddin Ali <hafiz89.ali@gmail.com>
2024-09-04 13:43:07 +0800

    Tidy Up Files

--- frontend/src/assets/components/Header.jsx
+++
@@ -1,5 +0,0 @@
-import { useNavigate } from "react-router-dom";
-
-function Header() {}
-
-export default Header;
--- frontend/src/assets/pages/LessonPlan.jsx
+++
@@ -1,9 +0,0 @@
-function LessonPlan() {
-  return (
-    <div>
-      <h1>LessonPlan</h1>
-    </div>
-  );
-}
-
-export default LessonPlan;
