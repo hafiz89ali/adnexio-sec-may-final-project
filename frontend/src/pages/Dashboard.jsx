58f4223226fedb234e4f4bf78028e9000b14ad71
Hafizuddin Ali <hafiz89.ali@gmail.com>
2024-09-05 09:45:24 +0800

    Dashboard Layout

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -12,6 +12,27 @@ function Dashboard() {
         </div>
         <div className="main-area">
           <p>Main Area</p>
+          <div className="sub-main1">
+            <div className="profile-sect">
+              <p>Profile Section</p>
+            </div>
+            <div className="comm-sect">
+              <p>Communication Section</p>
+            </div>
+          </div>
+          <div className="action-sect">
+            <p>Action Section</p>
+          </div>
+          <div className="content-area">
+            <p>Contents Area</p>
+            <h3>Rancangan Pengajaran Harian</h3>
+            <div>
+              <p>Sorting Area</p>
+            </div>
+            <div>
+              <p>Real Contents</p>
+            </div>
+          </div>
         </div>
       </body>
     </>
