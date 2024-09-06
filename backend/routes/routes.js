de39c17ed0b9587d3620c72f2f237d79d6688221
Hafizuddin Ali <hafiz89.ali@gmail.com>
2024-09-06 22:10:31 +0800

    rebuild backend

---
+++ backend/routes/routes.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import healthController from "../controllers/health.js";
+import authController from "../controllers/auth.js";
+import privacyController from "../controllers/privacy.js";
+import isAuth from "../middlewares/isAuth.js";
+
+const router = Router();
+
+router.get("/health", healthController.getHealth);
+router.post("/health", healthController.postHealth);
+router.post("/register", authController.registerUser);
+router.post("/login", authController.loginUser);
+router.get("/public", privacyController.publicPath);
+router.get("/private", isAuth, privacyController.privatePath);
+
+export default router;
