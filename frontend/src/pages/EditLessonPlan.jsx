c796fbdfc6808fca038540c8b66a8a59f7b986ff
Hafizuddin Ali <hafiz89.ali@gmail.com>
2024-09-12 07:14:27 +0800

    final update

--- frontend/src/pages/EditLessonPlan.jsx
+++ frontend/src/pages/EditLessonPlan.jsx
@@ -3,7 +3,6 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { getApiWithToken, putApiWithToken } from "../utils/api";
 import Cookies from "js-cookie";
-import dateFormat from "dateformat";
 import "../styles/viewLessonPlan.css";
 import iconHome from "../assets/iconHome.svg";
 import iconFileEdit from "../assets/iconFileEdit.svg";
@@ -13,46 +12,24 @@ function EditLessonPlan() {
   const [isLoading, setIsLoading] = useState(true);
   const [lessonPlan, setLessonPlan] = useState({});
   const { register, handleSubmit } = useForm();
+  const navigate = useNavigate();
 
-  async function fetchLinks() {
-    try {
-      setIsLoading(true);
-      const token = Cookies.get("authToken");
-      const serverRes = await getApiWithToken(
-        `http://localhost:3000/view/lessonplan/${id}`,
-        token
-      );
-      if (!serverRes.ok) {
-        alert("You are not authorized to view this page.");
-      }
-      const data = await serverRes.json();
-      setLessonPlan(data.data);
-    } catch {
-      console.error("Error at fetchLessonPlans");
-    } finally {
-      setIsLoading(false);
-    }
-  }
-
-  async function onSubmit(data) {
+  async function editLP(data) {
     try {
       const token = Cookies.get("authToken");
       const serverRes = await putApiWithToken(
-        `http://localhost:3000/update/lessonplan/${id}`,
-        data,
-        token
+        `http://localhost:3000/edit/lessonplan/${id}`,
+        token,
+        data
       );
       if (!serverRes.ok) {
-        const serverError = await serverRes.json();
-        const message = serverError.message;
-        alert(message);
-        throw new Error(message);
+        alert("You are not authorized to edit this lesson plan.");
+        return;
       }
-      const resData = await serverRes.json();
-      const message = resData.message;
-      alert(message);
+      alert("Lesson plan edited successfully");
     } catch (error) {
-      console.error(error);
+      console.error("Error at editLP");
+      alert("Error at editLP");
     }
   }
 
@@ -60,10 +37,6 @@ function EditLessonPlan() {
     editLP(data);
     console.log(data);
   }
-
-  useEffect(function () {
-    fetchLinks();
-  }, []);
 
   return (
     <div className="viewLessonPlan">
@@ -82,20 +55,55 @@ function EditLessonPlan() {
       </div>
       {/* <h1>View Lesson Plan</h1> */}
       <form onSubmit={handleSubmit(onSubmit)} id="newLP" className="newLP">
+        <label for="title">Title:</label>
         <input
           {...register("title")}
+          defaultValue={lessonPlan.title}
           type="text"
+          id="title"
           name="title"
-          value={lessonPlan.title}
         />
-        <h1>{lessonPlan.title}</h1>
-        <div className="lessonPlanDetails">
-          <p>{lessonPlan.subject_name} .</p>
-          <p>{lessonPlan.class_name} .</p>
-          <p>{dateFormat(lessonPlan.date, "dd/mm/yyyy")}</p>
-          <p>{lessonPlan.time}</p>
-        </div>
-        <p>{lessonPlan.lesson_plan}</p>
+        <label for="date">Date:</label>
+        <input
+          {...register("date")}
+          defaultValue={lessonPlan.date}
+          type="date"
+          id="date"
+          name="date"
+        />
+        <label for="time">Time:</label>
+        <input
+          {...register("time")}
+          defaultValue={lessonPlan.time}
+          type="time"
+          id="time"
+          name="time"
+        />
+        <label for="class_name">Class Name:</label>
+        <input
+          {...register("className")}
+          defaultValue={lessonPlan.class_name}
+          type="text"
+          id="className"
+          name="className"
+        />
+        <label for="subject_name">Subject Name:</label>
+        <input
+          {...register("subjectName")}
+          defaultValue={lessonPlan.subject_name}
+          type="text"
+          id="subjectName"
+          name="subjectName"
+        />
+        <label for="lesson_plan_text">Lesson Plan:</label>
+        <textarea
+          {...register("lessonPlan")}
+          defaultValue={lessonPlan.lesson_plan}
+          id="lessonPlan"
+          name="lessonPlan"
+          rows="4"
+        />
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
